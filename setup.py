9748a9a4712c59168300be51626bb8cd347dcf49
Michal Charemza <michal@charemza.name>
2018-11-23 22:52:47 +0000

    (release) v0.0.3

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ def long_description():
 
 setuptools.setup(
     name='fifolock',
-    version='0.0.2',
+    version='0.0.3',
     author='Michal Charemza',
     author_email='michal@charemza.name',
     description='A flexible low-level tool to make synchronisation primitives in asyncio Python',
