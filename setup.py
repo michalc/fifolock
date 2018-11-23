c82a07889e345c1f7505d2ba2c230ec7f6862924
Michal Charemza <michal@charemza.name>
2018-11-23 21:24:57 +0000

    (release) v0.0.1

---
+++ setup.py
@@ -0,0 +1,28 @@
+import setuptools
+
+
+def long_description():
+    with open('README.md', 'r') as file:
+        return file.read()
+
+
+setuptools.setup(
+    name='fifolock',
+    version='0.0.1',
+    author='Michal Charemza',
+    author_email='michal@charemza.name',
+    description='A flexible low-level tool to make synchronisation primitives in asyncio Python',
+    long_description=long_description(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/michalc/fifolock',
+    py_modules=[
+        'fifolock',
+    ],
+    python_requires='>=3.7',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Framework :: AsyncIO',
+    ],
+)
